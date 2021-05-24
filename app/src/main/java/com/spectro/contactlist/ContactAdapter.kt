62696e40378efcf57e173db248247df4991736a9
Guilherme Goulart <guigagoulart@hotmail.com>
2021-05-24 18:57:27 -0300

    criado options menu e algumas adaptações para que seja carregado o recyclerview

--- app/src/main/java/com/spectro/contactlist/ContactAdapter.kt
+++ app/src/main/java/com/spectro/contactlist/ContactAdapter.kt
@@ -3,6 +3,8 @@ package com.spectro.contactlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>(){
@@ -10,7 +12,8 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     private val list: MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate()
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
@@ -21,11 +24,20 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         return list.size
     }
 
+    fun updateList(list: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder (itemView){
+        private val tvName: TextView = itemView.findViewById(R.id.tv_mame)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
 
         fun bind(contact: Contact){
-
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
-
     }
 }
